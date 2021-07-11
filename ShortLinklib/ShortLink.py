62f29450573291eb27c9cb37d8c0b993226b045f
aa-5 <86760749+aa-5@users.noreply.github.com>
2021-07-11 11:38:12 +0530

    Code update

--- ShortLinklib/ShortLink.py
+++ ShortLinklib/ShortLink.py
@@ -1,8 +1,31 @@
 import os # PATH operations.
 from urllib.parse import urlparse # URL validation and others.
 import webbrowser # Open link in new tab.
-import ShorLinklib.validate_url as v
+import validators
 
 
 def url_validate(url):
-    return v.url_validate(url)
+    isValid = validators.url(url)
+    if isValid == True:
+        return True
+    else:
+        return False
+
+def open_in_browser(url):
+    webbrowser.new_taab(url)
+
+def save_(db_path,data):
+    x = open(db_path,'r')
+    x_dat = x.read()
+    x.close()
+    dat_ = x_dat.update(data)
+    f = open(db_path,'w')
+    f.write(dat_)
+    f.close()
+
+def read_(db_path):
+    x = open(db_path, 'r')
+    dat = x.read()
+    x.close()
+    return dat
+
