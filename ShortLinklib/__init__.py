3bc1bb5afcd0888685263dd07c0d1f74b91be5f9
AA <86760749+aerocyber@users.noreply.github.com>
2021-07-16 18:23:56 +0530

    Update __init__.py

--- ShortLinklib/__init__.py
+++ ShortLinklib/__init__.py
@@ -12,5 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import ShortLinklib.ShortLink
-import ShortLinklib.validate_url
+import ShortLink
